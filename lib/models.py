3d0b51ec16e25318917b90ba2e23cb78ee33f825
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2023-09-04 14:25:26 +0300

    Renamed models names

--- lib/models.py
+++ lib/models.py
@@ -21,36 +21,38 @@ session = Session()
 
 
 
-class Restaurants(Base):
+class Restaurant(Base):
     __tablename__ = 'restaurants'
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
     price=Column(Integer)
 
-    def __repr__(self):
-        return f"name:: {self.name}: price::{self.price}"
+    # def __repr__(self):
+    #     return f"name:: {self.name}: price::{self.price}"
 
-    reviews = relationship('Reviews', back_populates='restaurant')
+    reviews = relationship('Review', back_populates='restaurant')
     customers = association_proxy('reviews', 'customer', 
         creator = lambda cu: Reviews(customer = cu))
 
     def reviews(self):
+        # query = session.query(Reviews)
+        # print((query))
         return self.reviews
 
     def customers(self):
-        return [review.customer for review in self.reviews]
+        return self.customers
 
 
 
-class Customers(Base):
+class Customer(Base):
     __tablename__ = 'customers'
 
     id = Column(Integer, primary_key=True)
     first_name = Column(String)
     last_name = Column(String)
 
-    reviews = relationship('Reviews', back_populates='customer')
+    reviews = relationship('Review', back_populates='customer')
     restaurants = association_proxy('reviews', 'restaurant',
         creator=lambda re: Reviews(restaurant = re))
 
@@ -61,7 +63,7 @@ class Customers(Base):
         return [review.restaurant for review in self.reviews]
 
 
-class Reviews(Base):
+class Review(Base):
     __tablename__ = 'reviews'
 
     id=Column(Integer(), primary_key=True)
@@ -69,15 +71,19 @@ class Reviews(Base):
     restaurant_id = Column(Integer(), ForeignKey('restaurants.id'))
     customer_id = Column(Integer(), ForeignKey('customers.id'))
 
-    restaurant = relationship('Restaurants', back_populates='reviews')
-    customer = relationship('Customers', back_populates='reviews')
+    restaurant = relationship('Restaurant', back_populates='reviews')
+    customer = relationship('Customer', back_populates='reviews')
 
 
     def __repr__(self):
         return f"id={self.id}, rating = {self.star_rating} customer={self.customer_id} for restaurant::{self.restaurant_id}"
 
     def customer(self):
-        return self.customer
+        query = session.query(Customer).filter_by(id = self.customer_id).first()
+        return query
 
     def restaurant(self):
         return self.restaurant
+
+
+review1 = Review()
--- lib/seed.py
+++ lib/seed.py
@@ -1,16 +1,17 @@
 from faker import Faker
 import random
-from models import engine, session, Restaurants, Customers, Reviews
+from models import engine, session, Restaurant, Customer, Review
 
 if __name__ == '__main__':
-    session.query(Restaurants).delete()
-    session.query(Customers).delete()
+    session.query(Restaurant).delete()
+    session.query(Customer).delete()
+    session.query(Review).delete()
 
     fake = Faker()
 
     restaurants = []
     for j in range(50):
-        restaurant = Restaurants(
+        restaurant = Restaurant(
             name=fake.unique.company(),
             price=random.randint(1000, 50000)
         )
@@ -21,7 +22,7 @@ if __name__ == '__main__':
 
     customers = []
     for i in range(50):
-        customer = Customers(
+        customer = Customer(
             first_name=fake.first_name(),
             last_name=fake.last_name()
         )
@@ -36,26 +37,22 @@ reviews = []
 star_ratings = [0, 1, 2, 3, 4, 5]
 
 for restaurant in restaurants:
-    for i in range(random.randint(1,5)):
-
+    for i in range(random.randint(1,50)):
         customer = random.choice(customers)
-        if restaurant not in customer.restaurants:
-            customer.restaurants.append(restaurant)
-            session.add(customer)
-            session.commit()
+    
 
         star_rating = random.choice(star_ratings)
         print(f"Star Rating: {star_rating}")
 
-        review = Reviews(
-            star_rating=star_rating,
-            restaurant_id=restaurant.id,
-            customer_id=customer.id
-        )
+        review = Review(
+                star_rating=star_rating,
+                restaurant_id=restaurant.id,
+                customer_id=customer.id
+            )
         reviews.append(review)
 
 # for review in reviews:
-session.add(review)
+    session.add(review)
 
 
 session.commit()
